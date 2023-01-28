e5aecd5cf18e3e48caebf80dc73150a1d1f84ff8
Njonge9 <frednjonge9@gmail.com>
2023-01-28 11:11:39 +0300

    Write a valid email test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -13,4 +13,27 @@ class UserTest < ActiveSupport::TestCase
   @user.name = "   "
   assert_not @user.valid?
  end
+
+ test "email should be present" do
+  @user.email = " "
+  assert_not @user.valid?
+ end
+
+ test "name should not be too long" do
+  @user.name = "a" * 51
+  assert_not @user.valid?
+ end
+
+ test "email should not be too long" do
+  @user.email = "a" * 244 + "@example.com"
+  assert_not @user.valid?
+
+  test "email validation should accept valid addresses" do
+    valid_addresses = %w[user@example.com USER@foo.com A_US-ER@foo.bar.org first.last@foo.jp alice+bob@baz.cn]
+    valid_addresses.each do |valid_address|
+      assert @user.valid?, "#{valid_address.inspect} should be valid"
+    end
+  end
+ end
+
 end
