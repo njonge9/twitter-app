b735d193171d48f0ffad59569a114e4d39f39509
Njonge9 <frednjonge9@gmail.com>
2023-01-31 11:13:04 +0300

    Test for invalid submission

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
     else
-      render 'new', status: unprocessable_entity
+      render 'new', status: :unprocessable_entity
     end
   end
 
